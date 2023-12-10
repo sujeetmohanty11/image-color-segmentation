b52fcc86247245c2afb6a2809823dcaa506b7931
Sujeet <sujeetmohanty11@gmail.com>
2023-12-11 01:23:31 +0530

    added functionality to upload a file and save it in data/raw folder and display that image

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
 import streamlit as st
+import os
+from PIL import Image
+
+
+@st.cache_data
+def load_image(photo):
+    return Image.open(photo)
+
 
 st.header('Image Color Segmentation')
 st.write('A simple app to see top 5 most dominating colors in your image')
 
-file = st.file_uploader(label='upload an image')
+image_file = st.file_uploader(label='upload an image', type=['jpeg', 'png', 'jpg'])
+
+if image_file is not None:
+    file_details = {"FileName": image_file.name, "FileType": image_file.type}
+    img = load_image(image_file)
+    st.image(img, width=250)
+
+    with open(os.path.join("data/raw/", image_file.name), "wb") as f:
+        f.write(image_file.getbuffer())
+    st.success('Successfully Uploaded !')
