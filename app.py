99801f28a061d2cfafbed0b4935a94172d549208
Sujeet <sujeetmohanty11@gmail.com>
2023-12-23 22:54:09 +0530

    Fix: added few logics to make code work

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import os
 from PIL import Image
+import cv2
+from models.main import predict_color
 
 
 @st.cache_data
@@ -18,6 +20,13 @@ if image_file is not None:
     img = load_image(image_file)
     st.image(img, width=250)
 
-    with open(os.path.join("data/raw/", image_file.name), "wb") as f:
+    # with open(os.path.join("data/raw/", image_file.name), "wb") as f:
+    with open(os.path.join("data/raw/colors.jpg"), "wb") as f:  # saving uploaded file with name -> colors.jpg in data/
         f.write(image_file.getbuffer())
     st.success('Successfully Uploaded !')
+
+st.subheader("Top 5 most dominant colors are: ")
+pic = cv2.imread('color.jpg')
+predict_color(pic)
+
+st.image('output.jpg')
--- models/main.py
+++ models/main.py
@@ -4,62 +4,62 @@ import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 import cv2
 from sklearn.preprocessing import StandardScaler
+import warnings
+
+warnings.filterwarnings('ignore')
 
 
-image = cv2.imread('colors.jpg')
-scaler = StandardScaler(with_mean=0, with_std=1)
+def predict_color(image, no_of_clusters=5):
+    scaler = StandardScaler(with_mean=0, with_std=1)
 
-r = []
-g = []
-b = []
+    r = []
+    g = []
+    b = []
 
-for line in image:
-    for pixel in line:
-        temp_b, temp_g, temp_r = pixel
+    for line in image:
+        for pixel in line:
+            temp_b, temp_g, temp_r = pixel
 
-        r.append(temp_r)
-        g.append(temp_g)
-        b.append(temp_b)
+            r.append(temp_r)
+            g.append(temp_g)
+            b.append(temp_b)
 
-df = pd.DataFrame({'red': r, 'green': g, 'blue': b})
+    df = pd.DataFrame({'red': r, 'green': g, 'blue': b})
 
-df['scaled_red'] = scaler.fit_transform(df[['red']])
-df['scaled_green'] = scaler.fit_transform(df[['green']])
-df['scaled_blue'] = scaler.fit_transform(df[['blue']])
+    df['scaled_red'] = scaler.fit_transform(df[['red']])
+    df['scaled_green'] = scaler.fit_transform(df[['green']])
+    df['scaled_blue'] = scaler.fit_transform(df[['blue']])
 
-X = df[['scaled_red', 'scaled_green', 'scaled_blue']].values
+    X = df[['scaled_red', 'scaled_green', 'scaled_blue']].values
 
-SSE = []
+    for cluster in range(2, 8):
+        kmeans = KMeans(n_clusters=cluster, random_state=42)
+        kmeans.fit(X)
 
-for cluster in range(2, 8):
-    kmeans = KMeans(n_clusters=cluster, random_state=42)
+        kmeans.predict(X)
+
+    kmeans = KMeans(n_clusters=no_of_clusters)
     kmeans.fit(X)
+    kmeans.predict(X)
 
-    pred_clusters = kmeans.predict(X)
-    SSE.append(kmeans.inertia_)
+    # These are the centroids of the clusters
+    cluster_centers = kmeans.cluster_centers_
 
-frame = pd.DataFrame({'Cluster': range(2, 8), 'SSE': SSE})
+    colors = []
 
-k = 4
-kmeans = KMeans(n_clusters=k)
-kmeans.fit(X)
-k_pred = kmeans.predict(X)
+    r_std, g_std, b_std = df[['red', 'green', 'blue']].std()
 
-# These are the centroids of the clusters
-cluster_centers = kmeans.cluster_centers_
+    for cluster_center in cluster_centers:
+        scaled_r, scaled_g, scaled_b = cluster_center
 
-colors = []
+        colors.append((
+            scaled_r * r_std / 255,
+            scaled_g * g_std / 255,
+            scaled_b * b_std / 255
+        ))
 
-r_std, g_std, b_std = df[['red', 'green', 'blue']].std()
+    plt.imshow([colors])
+    plt.savefig('output.jpg')
 
-for cluster_center in cluster_centers:
-    scaled_r, scaled_g, scaled_b = cluster_center
 
-    colors.append((
-        scaled_r * r_std / 255,
-        scaled_g * g_std / 255,
-        scaled_b * b_std / 255
-    ))
 
-plt.imshow([colors])
-plt.show()
